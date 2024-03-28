4a5306750569fc8d6b4fd2364d93d4b37eb1e97f
Starlin Cerda <starlingjavierc@hotmail.com>
2024-03-27 20:39:50 -0400

    adding requirements and changes to run file

--- run.py
+++ run.py
@@ -5,4 +5,4 @@ from src import create_app
 
 if __name__ == '__main__':
 	app = create_app()
-	app.run(host='0.0.0.0', port=8000, debug=True, use_reloader=True)
+	#app.run(host='0.0.0.0', port=8000, debug=True, use_reloader=True)
