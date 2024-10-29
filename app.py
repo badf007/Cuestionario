cab4457b7b1dc5679665eeb428e7780569d03cf4
spi-pucmm <projecto_spi@outlook.com>
2024-10-29 18:07:09 -0400

    Lates changes

--- app.py
+++ app.py
@@ -1,5 +1,5 @@
 import os
-from flask import (Flask, render_template, redirect,url_for,request)
+from flask import (Flask, render_template, redirect,url_for,request, flash)
 import pandas as pd
 import gspread
 
@@ -49,6 +49,7 @@ def q2():
 #authorization
 #gc = pygsheets.authorize(service_file='src\cuestionario-418210-94eee3a815de.json')
 gc = gspread.service_account('/etc/secrets/connect.json')
+#gc = gspread.service_account('src\connect.json')
 
 
 @app.route('/store_q1', methods=['POST'])
@@ -70,7 +71,20 @@ def store_q1():
                 'pregunta 8':[request.form['q8']]}
         # Create empty dataframe
         df = pd.DataFrame(data=data)
+        
+        calc = int(df['pregunta 1'][0]) + int(df['pregunta 2'][0]) + int(df['pregunta 3'][0]) + int(df['pregunta 4'][0]) + int(df['pregunta 5'][0]) + int(df['pregunta 6'][0]) + int(df['pregunta 7'][0]) + int(df['pregunta 8'][0])
 
+        test_result = ""
+        
+        if calc >= 0 and calc <= 7:
+            test_result = "0-7: Es poco probable que tenga un sueño anormal."
+        elif calc >= 8 and calc <= 9:
+            test_result = "8-9: Tiene una cantidad promedio de somnolencia diurna."
+        elif calc >= 10 and calc <= 15:
+            test_result = "10-15: Puede que tengas demasiado sueño dependiendo de la situación. Quizás quieras considerar buscando atención médica."
+        elif calc >= 16 and calc <= 24:
+            test_result = "16-24: Tienes demasiado sueño y deberías considerar buscar atención médica."
+            
         full_read = gc.open_by_url('https://docs.google.com/spreadsheets/d/1S9yUmxvo6djOpAWKamh3sHzf2Wi1VJVUezPytuVixsc/edit#gid=0')
         sheet = full_read.get_worksheet(0)
         dataframe = pd.DataFrame(sheet.get_all_records())
@@ -89,6 +103,8 @@ def store_q1():
         error="Invalid information"
 
     #data = fetch()
+    
+    flash(test_result)
     return redirect(url_for('index'))
     #return render_template('e1.html'  )
 
@@ -111,6 +127,22 @@ def store_q2():
                 'pregunta 8':[request.form['q8']]}
         # Create empty dataframe
         df = pd.DataFrame(data=data)
+        
+        
+        responses = df.values.tolist()[0]
+        ccount = int(responses.count('SI'))
+        print(ccount)
+
+        
+        test_result = ""
+        
+        if ccount >= 0 and ccount <= 2:
+            test_result = "0-2: Bajo riesgo de AOS (Apnea Obstructiva del Sueño)"
+        elif ccount >= 3 and ccount <= 4:
+            test_result = "3-4: Riesgo intermedio de AOS (Apnea Obstructiva del Sueño)"
+        elif ccount >= 5 and ccount <= 8:
+            test_result = "5-8: Alto riesgo de AOS (Apnea Obstructiva del Sueño)"
+
 
         full_read = gc.open_by_url('https://docs.google.com/spreadsheets/d/17B8jnUJ6xLFBALR8uBNVHhP3Ip0AO19_lkpDT4oHxHE/edit#gid=0')
         sheet = full_read.get_worksheet(0)
@@ -130,6 +162,7 @@ def store_q2():
         error="Invalid information"
 
     #data = fetch()
+    flash(test_result)
     return redirect(url_for('index'))
 
 
